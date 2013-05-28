569b3090b64b1f10161c487ed0de237086298c89
Duane DeCouteau <duane.decouteau@gmail.com>
2013-05-28 13:29:26 -0600

    Initial commit Phase II VA/SAMHSA DS4P Pilot VA Developed Code

---
+++ ds4p-common-library/common-library/src/main/java/gov/va/ds4p/cas/providers/UserProfileProvider.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.va.ds4p.cas.providers;
+
+import gov.va.ds4p.auth.profile.UserProfile;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author Duane DeCouteau
+ */
+public class UserProfileProvider {
+    
+    public UserProfile createUserProfileObjectFromXML(String xml) {
+        UserProfile obj = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(UserProfile.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            StringReader sr = new StringReader(xml);
+
+            Object o = unmarshaller.unmarshal(sr);
+            obj = (UserProfile)o;
+
+        }
+        catch (Exception e) {
+            //log.warn("",e);
+            e.printStackTrace();
+        }        
+        return obj; 
+    }
+    
+    public String createXMLFromUserProfileObject(UserProfile obj) {
+        String res = "";
+        
+        try {
+            JAXBContext context = JAXBContext.newInstance(UserProfile.class);
+            Marshaller marshaller = context.createMarshaller();
+            StringWriter sw = new StringWriter();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            marshaller.marshal(obj, sw);
+
+            res = sw.toString();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        return res;
+        
+    }
+}
